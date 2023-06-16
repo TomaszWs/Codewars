f391ed4d3c10f234ad8e23b18f51994c2bae0258
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-06-16 12:45:09 +0200

    add codewars challenge

---
+++ 8kyu/Returning-Strings.py
@@ -0,0 +1,20 @@
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+# My solution
+
+def greet(name):
+    z = "Hello, {} how are you doing today?".format(name)
+    return z
+
+name = 'IMIE'
+print(greet(name))
+
+# Best practices
+
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
+
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
