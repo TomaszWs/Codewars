6c82544030b599c409dd16d4e9dd392f196593b5
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-06-25 10:56:24 +0200

    add codewars challenge

---
+++ 7kyu/List-Filtering.py
@@ -0,0 +1,25 @@
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+# Example
+
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+# My solution
+
+def filter_list(l):
+    new_list = []
+    for i in l:
+        if not isinstance(i, str): new_list.append(i)
+    return new_list
+
+l = [1,'a','b',0,15]
+print(filter_list(l))
+
+# Best practices
+
+def filter_list(l):
+  return [i for i in l if not isinstance(i, str)]
+
+def filter_list(l):
+  return [x for x in l if type(x) is not str]
