36c4bdc5ae0fe771b8d459176d24330531a4d4e8
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-11-08 11:15:43 +0100

    add codewars challenge

---
+++ 8kyu/Grasshopper-Messi-goals-function.py
@@ -0,0 +1,36 @@
+# Messi goals function
+#
+# Messi is a soccer player with goals in three leagues:
+#
+#     LaLiga
+#     Copa del Rey
+#     Champions
+#
+# Complete the function to return his total number of goals in all three
+# leagues.
+# Note: the input will always be valid.
+#
+# For example:
+#
+# 5, 10, 2  -->  17
+
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga+copaDelRey+championsLeague
+
+
+laLiga = 0
+copaDelRey = 0
+championsLeague = 0
+print(goals(laliga,copaDelRey,championsLeague))
+
+
+# Best solutions
+
+
+def goals(*a):
+    return sum(a)
+
+
+def goals(*args):
+    return sum(args)
