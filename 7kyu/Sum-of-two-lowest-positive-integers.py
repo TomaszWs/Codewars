d9c426bd489930edca663c81d36840a16dfc71c7
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-11-22 14:48:11 +0100

    add codewars challenge

---
+++ 7kyu/Sum-of-two-lowest-positive-integers.py
@@ -0,0 +1,29 @@
+# Create a function that returns the sum of the two lowest positive numbers
+# given an array of minimum 4 positive integers. No floats or non-positive
+# integers will be passed.
+#
+# For example, when an array is passed like [19, 5, 42, 2, 77], the output
+# should be 7.
+#
+# [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    return numbers[0]+numbers[1]
+
+
+numbers = [10, 343445353, 3453445, 3453545353453]
+print(sum_two_smallest_numbers(numbers))
+
+# Best solutions:
+
+
+def sum_two_smallest_numbers(numbers):
+    return sum(sorted(numbers)[:2])
+
+
+def sum_two_smallest_numbers(num_list):
+    num_list.sort()
+    return num_list[0] + num_list[1]
+
