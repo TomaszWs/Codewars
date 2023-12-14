591b0387aef10525551f6f897af66af46d58ddbd
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-12-14 10:31:18 +0100

    add codewars challenge

---
+++ 8kyu/Welcome.py
@@ -0,0 +1,138 @@
+# Your start-up's BA has told marketing that your website has a large audience
+# in Scandinavia and surrounding countries. Marketing thinks it would be great
+# to welcome visitors to the site in their own language. Luckily you already
+# use an API that detects the user's location, so this is an easy win.
+# The Task
+#
+#     Think of a way to store the languages as a database. The languages are
+#     listed below so you can copy and paste!
+#     Write a 'welcome' function that takes a parameter 'language', with a
+#     type String, and returns a greeting - if you have it in your database.
+#     It should default to English if the language is not in the database, or
+#     in the event of an invalid input.
+#
+# The Database
+#
+# [ ("english", "Welcome")
+# , ("czech", "Vitejte")
+# , ("danish", "Velkomst")
+# , ("dutch", "Welkom")
+# , ("estonian", "Tere tulemast")
+# , ("finnish", "Tervetuloa")
+# , ("flemish", "Welgekomen")
+# , ("french", "Bienvenue")
+# , ("german", "Willkommen")
+# , ("irish", "Failte")
+# , ("italian", "Benvenuto")
+# , ("latvian", "Gaidits")
+# , ("lithuanian", "Laukiamas")
+# , ("polish", "Witamy")
+# , ("spanish", "Bienvenido")
+# , ("swedish", "Valkommen")
+# , ("welsh", "Croeso")
+# ]
+#
+# Possible invalid inputs include:
+#
+# IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+# IP_ADDRESS_NOT_FOUND - ip address not in the database
+# IP_ADDRESS_REQUIRED - no ip address was supplied
+
+
+def greet(language):
+    data = [("english", "Welcome")
+        , ("czech", "Vitejte")
+        , ("danish", "Velkomst")
+        , ("dutch", "Welkom")
+        , ("estonian", "Tere tulemast")
+        , ("finnish", "Tervetuloa")
+        , ("flemish", "Welgekomen")
+        , ("french", "Bienvenue")
+        , ("german", "Willkommen")
+        , ("irish", "Failte")
+        , ("italian", "Benvenuto")
+        , ("latvian", "Gaidits")
+        , ("lithuanian", "Laukiamas")
+        , ("polish", "Witamy")
+        , ("spanish", "Bienvenido")
+        , ("swedish", "Valkommen")
+        , ("welsh", "Croeso")
+     ]
+    for l,greeting in data:
+        if l == language: return greeting
+    return "Welcome"
+
+
+
+language = "czech"
+print(greet(language))
+
+# Best solutions:
+
+def greet(language):
+    return {
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'english': 'Welcome',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }.get(language, 'Welcome')
+
+
+db = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'}
+
+def greet(language):
+    return db.get(language, "Welcome")
+
+
+welcome_message = {
+    'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso',
+}
+
+def greet(language):
+    return welcome_message.get(language, welcome_message['english'])
