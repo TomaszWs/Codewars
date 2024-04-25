809f4f8bdfce7a147dd00126ff0979eb38c2ad8f
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-04-25 13:04:21 +0200

    add codewars challenge

---
+++ 8kyu/Remove-First-and-Last-Character.py
@@ -0,0 +1,22 @@
+# It's pretty straightforward. Your goal is to create a function that removes
+# the first and last characters of a string. You're given one parameter,
+# the original string. You don't have to worry about strings with less
+# than two characters.
+
+
+def remove_char(s):
+    return s[1:len(s)-1]
+
+
+print(remove_char('eloquent'))
+
+
+# Best solutions:
+
+
+def remove_char(s):
+    return s[1 : -1]
+
+
+def remove_char(s):
+    return '' if len(s) <= 2 else s[1:-1]
