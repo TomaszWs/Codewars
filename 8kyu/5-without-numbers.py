5f8171ba422c09c81ecc508b768fad580a24a3c6
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-06-08 09:47:44 +0200

    add codewars challenge

---
+++ 8kyu/5-without-numbers.py
@@ -0,0 +1,26 @@
+# Write a function that always returns 5
+#
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+#
+# Good luck :)
+
+
+def unusual_five():
+    return len("fives")
+
+
+if __name__ == "__main__":
+    print(unusual_five())
+
+
+# Best solutions:
+
+
+def unusual_five():
+    return 'Support Ukraine!'.index('r')
+
+
+def unusual_five():
+    return(len('aaaaa'))
+
+
