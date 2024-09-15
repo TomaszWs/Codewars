cd2629f47daac4fdf1eb5e8bc66d36f78bc317e8
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-09-15 10:36:58 +0200

    add codewars challenge

--- 8kyu/USD-CNY.py
+++ 8kyu/USD-CNY.py
@@ -1,10 +1,14 @@
-# Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+# Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The
+# input is the amount of USD as an integer, and the output should be a string
+# that states the amount of Yuan followed by 'Chinese Yuan'
 # Examples (Input -> Output)
 #
 # 15  -> '101.25 Chinese Yuan'
 # 465 -> '3138.75 Chinese Yuan'
 #
-# The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+# The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers
+# should be represented as a string with 2 decimal places. (e.g. "21.00" NOT
+# "21.0" or "21")
 
 
 def usdcny(usd):
@@ -16,7 +20,6 @@ if __name__ == '__main__':
 
 
 # Best solutions:
-
 
 
 COURSE = 6.75  # CNY == 1 USD
