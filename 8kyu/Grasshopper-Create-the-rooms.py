66f1234e6c978702c72d1fa3abd22793e55d9408
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-09-30 18:12:39 +0200

    add codewars challenge

---
+++ 8kyu/Grasshopper-Create-the-rooms.py
@@ -0,0 +1,20 @@
+# Escape the room
+#
+# You are creating an "Escape the room" game. The first step is to create a
+# hash table called rooms that contains all of the rooms of the game. There
+# should be at least 3 rooms inside it, each being a hash table with at least
+# three properties (e.g. name, description, completed).
+
+
+rooms = {"1": {"name": 1, "description": 1, "completed": 1},
+         "2": {"name": 1, "description": 1, "completed": 1},
+         "3": {"name": 1, "description": 1, "completed": 1}}
+
+
+if __name__ == '__main__':
+    print(rooms)
+
+
+# Best solutions:
+
+
