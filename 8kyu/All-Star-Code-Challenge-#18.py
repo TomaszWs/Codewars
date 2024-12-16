3d6a0ee1cd8c59908d3e85cd4d9df55905faefc3
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-12-16 08:42:32 +0100

    add codewars challenge

---
+++ 8kyu/All-Star-Code-Challenge-#18.py
@@ -0,0 +1,36 @@
+# Create a function that accepts a string and a single character, and returns
+# an integer of the count of occurrences the 2nd argument is found in the first
+# one.
+#
+# If no occurrences can be found, a count of 0 should be returned.
+#
+# ("Hello", 'o')  =>  1
+# ("Hello", 'l')  =>  2
+# ("", 'z')       =>  0
+#
+# Notes
+#
+#     The first argument can be an empty string
+#     In languages with no distinct character data type, the second argument
+#     will be a string of length 1
+
+
+def str_count(strng, letter):
+    return strng.count(letter)
+
+
+if __name__ == '__main__':
+    print(str_count('hello', 'l'))
+
+
+# Best solutions:
+
+
+def str_count(strng, letter):
+    counter = 0
+
+    for chr in strng:
+        if chr == letter:
+            counter += 1
+
+    return counter
