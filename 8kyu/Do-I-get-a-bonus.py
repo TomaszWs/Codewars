1143ebb3fc682ed8e384467d3e9ca842e5c53cf7
Tomasz Wąs <tomasz.p.was@gmail.com>
2024-12-24 08:01:36 +0100

    add codewars challenge

---
+++ 8kyu/Do-I-get-a-bonus.py
@@ -0,0 +1,39 @@
+# It's bonus time in the big city! The fatcats are rubbing their paws in
+# anticipation... but who is going to make the most money?
+#
+# Build a function that takes in two arguments (salary, bonus). Salary will be
+# an integer, and bonus a boolean.
+#
+# If bonus is true, the salary should be multiplied by 10. If bonus is false,
+# the fatcat did not make enough money and must receive only his stated salary.
+#
+# Return the total figure the individual will receive as a string prefixed with
+# "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Dart, Ruby,
+# Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+
+
+def bonus_time(salary, bonus):
+    return f"${salary * 10}" if bonus is True else f"${salary}"
+
+
+if __name__ == '__main__':
+    print(bonus_time(10000, True))
+
+
+# Best solutions:
+
+
+def bonus_time(salary, bonus):
+    return "${}".format(salary * (10 if bonus else 1))
+
+
+def bonus_time(salary, bonus):
+    return f"${salary * 10 if bonus else salary}"
+
+
+def bonus_time(salary, bonus):
+    #your code here
+    if bonus :
+        return "$" + str(salary * 10)
+    else:
+        return "$" + str(salary)
