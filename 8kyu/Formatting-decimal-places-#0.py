e0ceefa641689219df456b3b9d33d62600fca831
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-03-14 08:56:40 +0100

    add codewars challenge

---
+++ 8kyu/Formatting-decimal-places-#0.py
@@ -0,0 +1,26 @@
+# Each number should be formatted that it is rounded to two decimal places. You
+# don't need to check whether the input is a valid number because only valid
+# numbers are used in the tests.
+#
+# Example:
+# 5.5589 is rounded 5.56
+# -3.3424 is rounded -3.34
+
+
+def two_decimal_places(n):
+    return round(n, 2)
+
+
+if __name__ == '__main__':
+    print(two_decimal_places(4.659725356))
+
+
+# Best solutions:
+
+
+def two_decimal_places(n):
+    return float("{0:.2f}".format(n))
+
+
+def two_decimal_places(n):
+    return float("%.2f" % n)
