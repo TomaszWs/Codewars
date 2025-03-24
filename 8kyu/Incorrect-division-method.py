e6b3f0289919e93aed46467995cda2f4493cf388
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-03-24 08:34:49 +0100

    add codewars challenge

---
+++ 8kyu/Incorrect-division-method.py
@@ -0,0 +1,10 @@
+# This method, which is supposed to return the result of dividing its first
+# argument by its second, isn't always returning correct values. Fix it.
+
+
+def divide_numbers(x,y):
+    return x / y
+
+
+if __name__ == '__main__':
+    print(divide_numbers(9, 4))
