09836dbced553c913173b994969cf19ff7932de0
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-03-29 14:57:02 +0100

    add codewars challenge

---
+++ 8kyu/For-Twins-1-Types.py
@@ -0,0 +1,32 @@
+# Prolog:
+#
+# This kata series was created for friends of mine who just started to learn
+# programming. Wish you all the best and keep your mind open and sharp!
+# Task:
+#
+# Write a function that will accept two parameters: variable and type and check
+# if type of variable is matching type. Return true if types match or false if
+# not.
+# Examples:
+#
+# 42, "int"    --> True
+# "42", "int"  --> False
+
+
+def type_validation(variable, _type):
+    return type(variable).__name__ == _type
+
+
+if __name__ == '__main__':
+    print(type_validation(42, "int"))
+
+
+# Best solutions:
+
+
+def type_validation(variable, _type):
+    return _type in str(type(variable))
+
+
+def type_validation(variable, type):
+    return variable.__class__.__name__ == type
