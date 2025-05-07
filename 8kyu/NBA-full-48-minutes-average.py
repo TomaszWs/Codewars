86b1289ebae62e6223829f3486ebb950d85a7a90
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-05-07 10:31:23 +0200

    add codewars challenge

---
+++ 8kyu/NBA-full-48-minutes-average.py
@@ -0,0 +1,44 @@
+# An NBA game runs 48 minutes (Four 12 minute quarters). Players do not
+# typically play the full game, subbing in and out as necessary. Your job is to
+# extrapolate a player's points per game if they played the full 48 minutes.
+#
+# Write a function that takes two arguments, ppg (points per game) and mpg
+# (minutes per game) and returns a straight extrapolation of ppg per 48 minutes
+# rounded to the nearest tenth. Return 0 if 0.
+#
+# Examples:
+#
+# nba_extrap(12, 20) # 28.8
+# nba_extrap(10, 10) # 48
+# nba_extrap(5, 17) # 14.1
+# nba_extrap(0, 0) # 0
+#
+# Notes:
+# All inputs will be either be an integer or float.
+# Follow your dreams!
+
+
+def nba_extrap(ppg, mpg):
+    return round(ppg * 48 / mpg, 1) if mpg != 0 else 0
+
+
+if __name__ == '__main__':
+    print(nba_extrap(12, 20))
+
+
+# Best solutions:
+
+
+def nba_extrap(ppg, mpg):
+    return round(48.0 / mpg * ppg, 1) if mpg > 0 else 0
+
+
+def nba_extrap(ppg, mpg):
+    try:
+        return round(ppg/(mpg * 1.0)*48, 1)
+    except ZeroDivisionError:
+        return 0
+
+
+def nba_extrap(ppg, mpg):
+    return round(48*ppg/mpg, 1)
