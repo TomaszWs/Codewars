d31e13c6e3bdf23745c07b0a9cd34bc7a5376b40
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-06-22 13:19:23 +0200

    add codewars challenge

---
+++ 8kyu/pick-a-set-of-first-elements.py
@@ -0,0 +1,27 @@
+# Write a function to get the first element(s) of a sequence. Passing a
+# parameter n (default=1) will return the first n element(s) of the sequence.
+#
+# If n == 0 return an empty sequence []
+# Examples
+#
+# arr = ['a', 'b', 'c', 'd', 'e']
+# first(arr)    # --> ['a']
+# first(arr, 2) # --> ['a', 'b']
+# first(arr, 3) # --> ['a', 'b', 'c']
+# first(arr, 0) # --> []
+
+
+def first(seq, n=1):
+    return seq[:n]
+
+
+if __name__ == '__main__':
+    seq = ['a', 'b', 'c', 'd', 'e']
+    print(first(seq, 2))
+
+
+# Best solutions:
+
+
+def first(seq, n=1):
+    return seq[0:n]
