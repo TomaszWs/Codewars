b666248b38f7b689078eae4ef1a0f26e9af4a843
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-08-04 15:29:53 +0200

    add codewars challenge

---
+++ 8kyu/Holiday-VI-Shark-Pontoon.py
@@ -0,0 +1,59 @@
+# Your friend invites you out to a cool floating pontoon around 1km off the
+# beach. Among other things, the pontoon has a huge slide that drops you out
+# right into the ocean, a small way from a set of stairs used to climb out.
+#
+# As you plunge out of the slide into the water, you see a shark hovering in
+# the darkness under the pontoon... Crap!
+#
+# You need to work out if the shark will get to you before you can get to the
+# pontoon. To make it easier... as you do the mental calculations in the water
+# you either freeze when you realise you are dead, or swim when you realise you
+# can make it!
+#
+# You are given 5 variables;
+#
+#     sharkDistance = distance from the shark to the pontoon. The shark will
+#     eat you if it reaches you before you escape to the pontoon.
+#
+#     sharkSpeed = how fast it can move in metres/second.
+#
+#     pontoonDistance = how far you need to swim to safety in metres.
+#
+#     youSpeed = how fast you can swim in metres/second.
+#
+#     dolphin = a boolean, if true, you can half the swimming speed of the
+#     shark as the dolphin will attack it.
+#
+#     The pontoon, you, and the shark are all aligned in one dimension.
+#
+# If you make it, return "Alive!", if not, return "Shark Bait!".
+
+
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+    if dolphin:
+        shark_speed /= 2
+    return "Alive!" if ((pontoon_distance / you_speed)
+                        < (shark_distance / shark_speed)) else "Shark Bait!"
+
+
+if __name__ == '__main__':
+    print(shark(12, 50, 4, 8, True))
+
+
+# Best solutions:
+
+
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+    if dolphin:
+        shark_speed = shark_speed / 2
+
+    shark_eat_time = shark_distance / shark_speed
+    you_safe_time = pontoon_distance / you_speed
+
+    return "Shark Bait!" if you_safe_time > shark_eat_time else "Alive!"
+
+
+def shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin):
+    # Using the boolean to be used as 1 or 0 when multiplying
+    return "Alive!" if (pontoonDistance / youSpeed) < sharkDistance / (
+                sharkSpeed - (sharkSpeed * 0.5 * dolphin)) else "Shark Bait!"
