a0d8c9b81a28a57b1cb251618f0e07becba0359a
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-08-21 13:36:15 +0200

    add codewars challenge

---
+++ 8kyu/Grasshopper-Terminal-Game-Turn-Function.py
@@ -0,0 +1,40 @@
+# Terminal game turn function
+#
+# You are creating a text-based terminal version of your favorite board game.
+# In the board game, each turn has six steps that must happen in this order:
+# roll the dice, move, combat, get coins, buy more health, and print status.
+#
+# You are using a library (Game.Logic in C#) that already has the functions
+# below. Create a function named doTurn/DoTurn/do_turn that calls the functions
+# in the proper order as described in the paragraph above.
+#
+# - `combat`
+# - `buy_health`
+# - `get_coins`
+# - `print_status`
+# - `roll_dice`
+# - `move`
+
+
+def do_turn():
+    roll_dice()
+    move()
+    combat()
+    get_coins()
+    buy_health()
+    print_status()
+
+
+if __name__ == '__main__':
+    print(do_turn())
+
+
+# Best solutions:
+
+
+def do_turn():
+    steps = [roll_dice, move, combat,
+             get_coins, buy_health, print_status]
+
+    for step in steps:
+        step()
