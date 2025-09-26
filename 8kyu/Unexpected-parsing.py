5eed4f5921eabf12a7a9dffe17bdfffa2667a82b
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-09-26 16:42:28 +0200

    add codewars challenge

---
+++ 8kyu/Unexpected-parsing.py
@@ -0,0 +1,34 @@
+# Here is a piece of code:
+#
+# def get_status(is_busy):
+#     status = "busy" if is_busy else "available"
+#     return status
+#
+# Expected Behaviour
+#
+# Function should return a dictionary/Object/Hash with "status" as a key, whose
+# value can : "busy" or "available" depending on the truth value of the
+# argument is_busy.
+#
+# But as you will see after clicking RUN or ATTEMPT this code has several bugs,
+# please fix them.
+
+
+def get_status(is_busy):
+    return {"status": "busy" if is_busy else "available"}
+
+
+if __name__ == '__main__':
+    print(get_status(True))
+
+
+# Best solutions:
+
+
+def get_status(is_busy):
+    status = "busy" if is_busy else "available"
+    return {"status" : status}
+
+
+def get_status(is_busy):
+    return {"status": "busy"} if is_busy else {"status": "available"}
