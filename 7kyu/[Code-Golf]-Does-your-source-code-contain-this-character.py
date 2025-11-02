d717aeb279da20b0bb924e8665517af8626df3fe
Tomasz Wąs <tomasz.p.was@gmail.com>
2025-11-02 20:25:41 +0100

    add codewars challenge

---
+++ 7kyu/[Code-Golf]-Does-your-source-code-contain-this-character.py
@@ -0,0 +1,28 @@
+# Given a character, return True if your source code (the one from
+# solution tab) contains this character or False if it doesn't.
+# Example
+#
+# d('d')  # probably True, because the name of the function is 'd',
+# unless you obfuscate it
+#
+# Importing is disabled. You also may not use open('solution.py') to
+# fetch your code from the file system, that's cheating.
+#
+# Keep your code short (under 33 characters)
+# Restricted
+# Metaprogramming
+
+
+d=lambda d:d in r"\"d=:lamb rin"
+
+
+# Best solutions:
+
+
+d='\.count\'d='.count
+
+
+d='\'\\d=_contais.'.__contains__
+
+
+d=lambda d:d in'\'=am\\blind 3:'
